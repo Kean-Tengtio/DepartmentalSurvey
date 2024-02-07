126f6b28b5bf98c2e40f27bd060398c04eb69d38
MIS <MIS@DESKTOP-JHVB1I9>
2024-02-07 15:48:22 +0800

    Add project files.

---
+++ DepartmentalSurvey/Controllers/DepartmentController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DepartmentalSurvey.Models;
+
+namespace DepartmentalSurvey.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private DepartmentalSurveyEntities db = new DepartmentalSurveyEntities();
+
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View(db.Department.ToList());
+        }
+
+        // GET: Department/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Department/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Department/Create
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Department.Add(department);
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(department);
+            }
+        }
+
+        // GET: Department/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Department.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Department/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Department department)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(department).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Department/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Department/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DepartmentalSurvey/Controllers/SchoolYearController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DepartmentalSurvey.Models;
+
+namespace DepartmentalSurvey.Controllers
+{
+    public class SchoolYearController : Controller
+    {
+        private DepartmentalSurveyEntities db = new DepartmentalSurveyEntities();
+
+
+        // GET: SchoolYear
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SchoolYear/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SchoolYear/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SchoolYear/Create
+        [HttpPost]
+        public ActionResult Create(SchoolYear schoolyear)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.SchoolYear.Add(schoolyear);
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(schoolyear);
+            }
+        }
+
+        // GET: SchoolYear/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SchoolYear/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SchoolYear/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SchoolYear/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
